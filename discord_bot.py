eb63ce1946e6cb1b19fb6d93677fb2d02a853362
dinobenj <73138313+dinobenj@users.noreply.github.com>
2021-11-25 21:31:37 -0500

    Add files via upload

---
+++ discord_bot.py
@@ -0,0 +1,20 @@
+import discord
+import foreground_editor as f
+from discord.ext import commands
+
+
+bot = commands.Bot(command_prefix="!", case_insensitive=True)
+DISCORD_TOKEN='OTEzNTkzNzA3MzU2MzczMDIy.YaAwcQ.WkRd4NWHOwdCDPO2FlkBrndckQE'
+client = discord.Client()
+
+    
+@bot.command(name='hello', description="Greet the user!")
+async def hello(ctx):
+    await ctx.send(f"Hello {ctx.author.name}!") 
+@bot.command()
+async def upload_file(ctx, arg1,arg2):
+    print("argument recieved!")
+    print(arg1)
+    f.foreground_destroyer(arg1, int(arg2))
+    await ctx.send(file=discord.File("deez.png"))
+bot.run(DISCORD_TOKEN)
